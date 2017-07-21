dbf6cf3babd0e66851790dbc91d31541a7abdc6c
childhealth <mike.kelly1@nhs.net>
2017-07-21 17:02:15 +0100

    Update insert_eventtype.sql

--- sql/insert_eventtype.sql
+++ sql/insert_eventtype.sql
@@ -1,1 +1,10 @@
+/*
+ * SQL to insert test data in EventType table.
+ * Assumes table does already exist.
+ *
+ * MAK 21-07-2017: First version
+ */
+ 
+INSERT INTO EventType
+VALUES (', value2, value3, ...);
 
