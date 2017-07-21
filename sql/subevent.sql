58734574e99fc440fbdf8bc41b4df21b19539e92
childhealth <mike.kelly1@nhs.net>
2017-07-21 16:18:46 +0100

    Update subevent.sql

--- sql/subevent.sql
+++ sql/subevent.sql
@@ -1,1 +1,18 @@
+/*
+ * SQL to create table SubEvent and associated indexes.
+ * Assumes table does not already exist.
+ *
+ * MAK 21-07-2017: First version
+ */
+ 
+CREATE TABLE SubEvent(
+  EventType VARCHAR(20) NOT NULL,
+  SubID VARCHAR(20) NOT NULL,
+  CONSTRAINT PK_End PRIMARY KEY (EventType, SubID)
+);
 
+CREATE INDEX idx_type
+ON SubEvent (EventType);
+
+CREATE INDEX idx_id
+ON SubEvent (SubID);
