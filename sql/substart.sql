ccfd8ac4b1117cc7bb25232eb905b5ce4b55f475
childhealth <mike.kelly1@nhs.net>
2017-07-21 15:54:15 +0100

    Update substart.sql

--- sql/substart.sql
+++ sql/substart.sql
@@ -1,11 +1,11 @@
 CREATE TABLE SubStart(
   StartDateTime TIMESTAMP NOT NULL,
-  SubId VARCHAR(20) NOT NULL,
-  CONSTRAINT PK_Start PRIMARY KEY (StartDateTime, SubId)
+  SubID VARCHAR(20) NOT NULL,
+  CONSTRAINT PK_Start PRIMARY KEY (StartDateTime, SubID)
 );
 
 CREATE INDEX idx_datetime
 ON SubStart (StartDateTime);
 
 CREATE INDEX idx_id
-ON SubStart (SubId);
+ON SubStart (SubID);
