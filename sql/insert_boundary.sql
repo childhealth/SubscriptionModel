e5c734b9c63e790e9ee377acf34b127f45c216d1
childhealth <mike.kelly1@nhs.net>
2017-07-25 14:07:34 +0100

    Create insert_boundary.sql

---
+++ sql/insert_boundary.sql
@@ -0,0 +1,12 @@
+/*
+ * SQL to insert test data in Boundary table.
+ * Assumes table does already exist.
+ * 
+ * Data only for the first 1 boundaries is given.
+ * The boundaries and their definitions are not yet finalised, therefore this data should be treated as experimental
+ *
+ * MAK 25-07-2017: First version
+ */
+ 
+INSERT INTO Boundary
+VALUES ('LA1', 'Local Authority 1.');
