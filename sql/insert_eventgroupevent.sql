f763eb2e915964eafb0786cbc081d804c92f6beb
childhealth <mike.kelly1@nhs.net>
2017-07-25 13:55:46 +0100

    Update insert_eventgroupevent.sql

--- sql/insert_eventgroupevent.sql
+++ sql/insert_eventgroupevent.sql
@@ -11,3 +11,59 @@
 INSERT INTO EventGroupEvent
 VALUES ('UHV', 'ETYPE01');
 
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE02');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE03');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE04');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE05');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE06');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE07');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE08');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE09');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE10');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE11');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE12');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE13');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE14');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE15');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE16');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE17');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE18');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE19');
+
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE20');
