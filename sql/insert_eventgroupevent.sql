19b8e736e3a93f881a2e0afdaa1e7a7856d1a835
childhealth <mike.kelly1@nhs.net>
2017-07-25 13:52:16 +0100

    Create insert_eventgroupevent.sql

---
+++ sql/insert_eventgroupevent.sql
@@ -0,0 +1,13 @@
+/*
+ * SQL to insert test data in EventGroupEvent table.
+ * Assumes table does already exist.
+ * 
+ * Data only for the first 2 event groups is given.
+ * The event groups and their definitions are not yet finalised, therefore this data should be treated as experimental
+ *
+ * MAK 25-07-2017: First version
+ */
+ 
+INSERT INTO EventGroupEvent
+VALUES ('UHV', 'ETYPE01');
+
