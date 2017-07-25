04760550f7d91de3e6c130f893470a8d90624a41
childhealth <mike.kelly1@nhs.net>
2017-07-25 13:38:08 +0100

    Update and rename create_eventgroup.sql to create_eventgroupevent.sql

--- sql/create_eventgroupevent.sql
+++ sql/create_eventgroupevent.sql
@@ -1,11 +1,11 @@
 /*
- * SQL to create table EventGroup and associated indexes.
+ * SQL to create table EventGroupEvent and associated indexes.
  * Assumes table does not already exist.
  *
  * MAK 25-07-2017: First version
  */
  
-CREATE TABLE EventGroup(
+CREATE TABLE EventGroupEvent(
   EventGroupID VARCHAR(20) NOT NULL,
   EventTypeID VARCHAR(20) NOT NULL,
   CONSTRAINT PK_Event PRIMARY KEY (EventGroupID, EventTypeID)
